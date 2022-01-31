4e4669b11d9ed4d014d9a4d68fda36815179b5ab
GRTerpstra <gerwin_terpstra@live.nl>
2022-01-31 23:10:57 +0100

    First commit.

---
+++ src/data/items.js
@@ -0,0 +1,32 @@
+const items = [
+    {
+        id: 1,
+        name: "Comfy Slippers",
+        price: 12.00,
+        category: 'Footwear',
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit iste, laborum temporibus explicabo maiores delectus tempore a. Odio, ipsum perspiciatis.",
+    },
+    {
+        id: 2,
+        name: "Item2",
+        price: 24.00,
+        category: 'Footwear',
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit iste, laborum temporibus explicabo maiores delectus tempore a. Odio, ipsum perspiciatis.",
+    },
+    {
+        id: 3,
+        name: "Item3",
+        price: 20.00,
+        category: 'Footwear',
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit iste, laborum temporibus explicabo maiores delectus tempore a. Odio, ipsum perspiciatis.",
+    },
+    {
+        id: 4,
+        name: "Item4",
+        price: 36.00,               
+        category: 'Footwear',
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit iste, laborum temporibus explicabo maiores delectus tempore a. Odio, ipsum perspiciatis.",
+    },
+]
+
+export default items
